49143a2083d0f8ead0a6e7d9925310fb5e0bd0ba
Mitchel Alejo <michalejo94@gmail.com>
2023-01-10 12:56:03 -0600

    Feat:Created getCustomerByUserId function

--- db/customerUser.js
+++ db/customerUser.js
@@ -2,61 +2,93 @@ const client = require("./client");
 // const bcrypt = require('bcrypt')
 const SALT_COUNT = 10;
 
-async function createCustomerUser (fields){
-    if(!fields.username || !fields.password){
-        return;
-    }
-    const username = fields.username
-    const password = fields.password
+async function createCustomerUser(fields) {
+  if (!fields.username || !fields.password) {
+    return;
+  }
+  const username = fields.username;
+  const password = fields.password;
 
-    //create hash password
+  //create hash password
 
-    const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
 
-    try{
-        const {rows: [newCustomerUser]} = await client.query(`
+  try {
+    const {
+      rows: [newCustomerUser],
+    } = await client.query(
+      `
         INSERT INTO users (username, password)
         VALUES ($1, $2)
         RETURNING *
         ;
-        `, [username,hashedPassword]);
-          // ^might need to update where we are inserting to ^
-        delete newCustomerUser.password;
-        return newCustomerUser
-    }catch(error){
-        console.log("error creating newCustomer user, customerUser.js", error);
-        throw (error)
-    }
-
+        `,
+      [username, hashedPassword]
+    );
+    // ^might need to update where we are inserting to ^
+    delete newCustomerUser.password;
+    return newCustomerUser;
+  } catch (error) {
+    console.log("error creating newCustomer user, customerUser.js", error);
+    throw error;
+  }
 }
 
-async function getCustomerUser({username, password}){
-    try{
-        
-        const {rows: [fetchCustomerUser]} = await client.query (`
+async function getCustomerUser({ username, password }) {
+  try {
+    const {
+      rows: [fetchCustomerUser],
+    } = await client.query(
+      `
         SELECT * FROM users
         WHERE username=$1
         ;
-        `,[username])
-        // ^might need to update where we are selecting from ^
+        `,
+      [username]
+    );
+    // ^might need to update where we are selecting from ^
 
-        if(!fetchCustomerUser){
-            throw new ("username or password do not match")
-        }else{
-            const checkPassword = await bcrypt.compare(password, fetchCustomerUser.password)
-            if (checkPassword === true){
-                delete fetchCustomerUser.password
-                return fetchCustomerUser
-            }
-        }
-    }catch(error){
-        console.log("error fetching user ", error);
-        throw (error)
+    if (!fetchCustomerUser) {
+      throw new "username or password do not match"();
+    } else {
+      const checkPassword = await bcrypt.compare(
+        password,
+        fetchCustomerUser.password
+      );
+      if (checkPassword === true) {
+        delete fetchCustomerUser.password;
+        return fetchCustomerUser;
+      }
     }
+  } catch (error) {
+    console.log("error fetching user ", error);
+    throw error;
+  }
+}
 
+async function getCustomerUserById(customerUserId) {
+  try {
+    const {
+      rows: [getCustomerUserById],
+    } = await client.query(
+      `
+        SELECT FROM users
+        WHERE id=$1
+        ;
+        `,
+      [customerUserId]
+    );
+    // ^might need to update where we are selecting from ^
+    delete customerUserId.password;
+  } catch (error) {
+    console.log("Error getCustomerUserById", error);
+    throw error;
+  }
 }
 
 module.exports = {
-    createCustomerUser,
-    getCustomerUser
-}
+  createCustomerUser,
+  getCustomerUser,
+  getCustomerUserById,
+  
+};
