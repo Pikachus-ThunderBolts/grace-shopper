9208ef16d41c3f78f9ee3734586bbcaf53a7ba9b
Mitchel Alejo <michalejo94@gmail.com>
2023-01-14 09:28:26 -0600

    feat: customerUser.js implemented post and get functions

--- api/customerUser.js
+++ api/customerUser.js
@@ -9,3 +9,49 @@ const {
   getCustomerUserByUsername,
   getCustomerUserById,
 } = require("../db/customerUsers");
+
+//Router.get
+router.get("/", async (req, res) => {
+  res.send("Test");
+});
+
+//Router.post
+router.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Missing username or password",
+    });
+  }
+  try {
+    const user = getCustomerUserByUsername(username);
+    bcrypt.compare(password, user.password, (error) => {
+      if (error) {
+        next({
+          name: "UserAuthenticationError",
+          message: "username or password was incorrect",
+        });
+      } else {
+        const token = jwt.sign(
+          {
+            id: user.id,
+            username,
+          },
+          JWT_SECRET,
+          { expiresIn: "1w" }
+        );
+        res.json({
+          message: "you're logged in!",
+          token,
+          user: { id: user.id, username },
+        });
+      }
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+module.exports = router;
