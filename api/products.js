1abb50551ec8b64ae8c48b784a3bc9a68dee0844
Cindy Carrillo <carrillo201994@gmail.com>
2023-01-16 16:20:03 -0600

    feat(products): making additional routes

--- api/products.js
+++ api/products.js
@@ -1,5 +1,6 @@
 const apiRouter = require('express').Router();
 
+const { unstable_renderSubtreeIntoContainer } = require('react-dom');
 const {
     createNewProduct,
     getAllProductsById,
@@ -24,52 +25,55 @@ apiRouter.get("/", async (req, res, next) => {
 });
 
 // POST /api/products
-// apiRouter.post('/', async(req, res, next) => {
-//     const token = req.headers.authorization.slice(7);
-//     // const signedIn = jwt.verify(token);
-//     try {
-//         const {brand, title, description, price, quantity, category, img} = req.body;
-//         const creatorId = req.user.id;
-//         const productData = {creatorId, brand, title, description, price, quantity, category, img};
-//         const existingProduct = await getProductsByTitle({title});
+apiRouter.post('/', async(req, res, next) => {
+    const token = req.headers.authorization.slice(7);
+    // const signedIn = jwt.verify(token);
+    try {
+        const {brand, title, description, price, quantity, category, img} = req.body;
+        const creatorId = req.user.id;
+        const productData = {creatorId, brand, title, description, price, quantity, category, img};
+
+        const existingProduct = await getProductsByTitle(title);
         
 
-//         if(existingProduct) {
-//             next({
-//                 name: `ProductExistsError`,
-//                 message: `A product with title ${title} already exists`
-//             })
-//         } else {
-//             const newProduct = await createNewProduct({productData});
-//             if(newProduct) {
-//                 res.json(newProduct)
-//                 res.send(newProduct)
-//             }
-//         }
-//     } catch (error) {
-//         next(error);
-//     }
-// })
+        if(existingProduct) {
+            next({
+                name: `ProductExistsError`,
+                message: `A product with title ${title} already exists`
+            })
+        } else {
+            const newProduct = await createNewProduct(productData);
+            if(newProduct) {
+                res.json(newProduct)
+                res.send(newProduct)
+            }
+        }
+    } catch ({name, message}) {
+        next({name, message});
+    }
+})
 
 
-// apiRouter.post('/', async(req, res, next) => {
-//     try {
-//         const {brand, title, description, price, quantity, category, img} = req.body;
-//         const existingProduct = await getProductsBytitle(title);
-//         if(existingProduct) {
-//             next({
-//                 name: `ProductExistsError`,
-//                 message: `A product with title ${title} already exists`
-//             })
-//         } else {
-//             const createdProduct = await createdProduct({brand, title, description, price, quantity, category, img});
-//             if(createdProduct) {
-//                 res.send(createdProduct)
-//             }
-//         }
-//     } catch (error) {
-//         next(error);
-//     }
-// })
+// PATCH /api/products/:productId
+//gets product by id to be able to update product
+apiRouter.patch("/:productId", async (req, res, next) => {
+    if(!req.user)
+    res.status(401).send({
+        name: `InvalidCredentialError`,
+        message: `You must be logged in to perform this action`,
+    });
+    try {
+        const {brand, title, description, price, quantity, category, img} = req.body;
+        const adminId = req.user.id;
+        const productData = {adminId, brand, title, description, price, quantity, category, img};
+        const newProduct = await createNewProduct(productData);
+        res.json(newProduct);
+        res.send(newProduct);
+    } catch ({name, message}) {
+        next({name, message});
+    }
+});
+
+//DELETE /api/products/:productId
 
 module.exports = apiRouter;
