4dec150547fe404aabd6c6ed1576495087dc6000
Cindy Carrillo <carrillo201994@gmail.com>
2023-01-18 14:30:01 -0600

    feat(adminUsers): can now register new user

--- api/adminUsers.js
+++ api/adminUsers.js
@@ -13,7 +13,7 @@ const jwt = require("jsonwebtoken");
 
 
 
-const { getAllAdminUsers, getAdminUserById, getAdminUserByUsername } = require("../db/adminUsers");
+const { getAllAdminUsers, getAdminUserById, getAdminUserByUsername, createAdminUser } = require("../db/adminUsers");
 
 //Router.get
 apiRouter.get("/", async (req, res) => {
@@ -29,7 +29,6 @@ apiRouter.post("/login", async (req, res, next) => {
   const { username } = req.body;
   try {
     const  user  = await getAdminUserByUsername(username)
-    console.log('this is user:', user)
     const token = jwt.sign({
       id: user.id,
       username: user.username
@@ -45,6 +44,92 @@ apiRouter.post("/login", async (req, res, next) => {
     next(error);
   }
 });
+
+apiRouter.post('/register', async (req, res, next) => {
+  const { username, email, password } = req.body;
+  try {
+    const _user = await getAdminUserByUsername(username);
+    if (_user) {
+      res.send({
+        error: `user ${username} is already taken`,
+        name: 'UsernameDuplicate',
+        message: `User ${username} is already taken`,
+      })
+      return
+    } else if (password.length < 6) {
+      res.send({
+        error: "Password Too Short!",
+        name: "PasswordLengthError",
+        message: "Password Too Short!",
+      })
+      return
+    }
+    const newUser = await createAdminUser({ username, email, password });
+    const token = jwt.sign(
+      {
+        username,
+      },
+      JWT_SECRET,
+      { expiresIn: "1w" }
+    );
+
+    res.send({
+        message: 'New user created successfully.',
+        token: token,
+        user: newUser
+      })
+    
+  } catch (error) {
+    next(error);
+  }
+});
+
+// apiRouter.post("/register", async (req, res, next) => {
+//   const { username, password } = req.body;
+
+//   try {
+//     const _user = await getAdminUserByUsername(username);
+    
+//     if (_user) {
+//       res.send({
+//         error: `User ${username} is already taken.`,
+//         name: "UserDuplicated",
+//         message: `User ${username} is already taken.`,
+//       });
+//       return
+//     }
+
+//     if (password.length < 6) {
+//       res.send({
+//         error: "Password Too Short!",
+//         name: "PasswordLengthError",
+//         message: "Password Too Short!",
+//       });
+//       return
+//     }
+
+//     const  id  = await createAdminUser({ username, email, password });
+//     console.log('id:', id)
+
+//     const token = jwt.sign(
+//       {
+//         id: id, email,
+//         username, 
+//       },
+//       JWT_SECRET,
+//       { expiresIn: "1w" }
+//     );
+//     console.log(token, "this is token")
+
+//     res.send({
+//       message: "success",
+//       token,
+//       user: { id: id, username, email },
+//     });
+//   } catch ({ name, message }) {
+//     next({ name, message });
+//   }
+// });
 
 
 module.exports = apiRouter;
