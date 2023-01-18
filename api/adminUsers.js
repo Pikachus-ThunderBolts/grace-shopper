8c87d130113d07658530621def20f280d3f89534
Cindy Carrillo <carrillo201994@gmail.com>
2023-01-18 13:52:34 -0600

    feat(adminUsers): able to login user

--- api/adminUsers.js
+++ api/adminUsers.js
@@ -1,15 +1,46 @@
 const apiRouter = require("express").Router();
-const bcrypt = require("bcrypt");
+require('dotenv').config();
+// gets our secret files
 const { JWT_SECRET } = process.env;
+console.log(process.env.JWT_SECRET, "here is secret");
+
+// requiring for hashing and checking passwords
+const bcrypt = require("bcrypt");
+
+// required to build web tokens
 const jwt = require("jsonwebtoken");
 
-const { getAllAdminUsers, getAdminUserById } = require("../db/adminUsers");
+
+
+
+const { getAllAdminUsers, getAdminUserById, getAdminUserByUsername } = require("../db/adminUsers");
 
 //Router.get
 apiRouter.get("/", async (req, res) => {
   try {
     const adminUsers = await getAllAdminUsers();
     res.send(adminUsers);
+  } catch (error) {
+    next(error);
+  }
+});
+
+apiRouter.post("/login", async (req, res, next) => {
+  const { username } = req.body;
+  try {
+    const  user  = await getAdminUserByUsername(username)
+    console.log('this is user:', user)
+    const token = jwt.sign({
+      id: user.id,
+      username: user.username
+    }, JWT_SECRET)
+
+    res.send({
+      token,
+      user: user,
+      message: "you're logged in!",
+    })
+
   } catch (error) {
     next(error);
   }
