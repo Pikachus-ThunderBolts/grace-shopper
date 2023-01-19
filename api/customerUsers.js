9d465b9792e2fed029ece36fd67ef8f293ac7b5c
Mitchel Alejo <michalejo94@gmail.com>
2023-01-19 12:20:56 -0600

    feat: customer user me route

--- api/customerUsers.js
+++ api/customerUsers.js
@@ -82,8 +82,36 @@ apiRouter.post("/register", async (req, res, next) => {
   }
 });
 
-// api/customerUsers/:username/cart
 //api/customerUsers/me
+
+// apiRouter.get("/me", async (req, res, next) => {
+//   const prefix = "Bearer";
+//   const auth = req.headers.authorization;
+//   try {
+//     if (!auth) {
+//       res.status(401).send({
+//         error: "You must be logged in to perform this action",
+//         message: "You must be logged in to perform this action",
+//         name: "InvalidCredentialsError",
+//       });
+//     } else if (auth.startsWith(prefix)) {
+//       const token = auth.slice(prefix.length);
+//       const { id } = jwt.verify(token, JWT_SECRET);
+//       req.user = await getUserById(id);
+//       res.send(req.user);
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1pdGNoZWw5NCIsImlhdCI6MTY3NDE0ODAzMSwiZXhwIjoxNjc0NzUyODMxfQ.G4dsILWQgNGAQ7z5uPtb3zUXdZAd5NvqKJse6PC0-X8",
+//     "user": {
+//         "id": 7,
+//         "username": "mitchel94",
+//         "email": "mitchel@email.com"
+
+// api/customerUsers/:username/cart
 //placedOrders
 //reviews
 
