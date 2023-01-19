9bbe3841994743a92340d0517394f0b143d0b1f5
Martin Hampton <martinh0205@gmail.com>
2023-01-19 12:52:00 -0600

    register working w bugs

--- api/guestUsers.js
+++ api/guestUsers.js
@@ -1,11 +1,56 @@
 const apiRouter = require("express").Router();
+require(`dotenv`).config();
 
-const { getAllGuestUsers } = require("../db/guestUsers");
+const { JWT_SECRET } = process.env;
+
+const {
+  getAllGuestUsers,
+  getGuestUsersByUsername,
+  createGuestUsers,
+} = require("../db/guestUsers");
 
 apiRouter.get("/", async (req, res) => {
   try {
     const guestUsers = await getAllGuestUsers();
     res.send(guestUsers);
+  } catch (error) {
+    next(error);
+  }
+});
+
+apiRouter.post("/register", async (req, res, next) => {
+  const { email } = req.body;
+  try {
+    /*const _user = await getAdminUserByUsername(username);
+    if (_user) {
+      res.send({
+        error: `user ${username} is already taken`,
+        name: 'UsernameDuplicate',
+        message: `User ${username} is already taken`,
+      })
+      return
+    } else if (password.length < 6) {
+      res.send({
+        error: "Password Too Short!",
+        name: "PasswordLengthError",
+        message: "Password Too Short!",
+      })
+      return
+    }
+    */
+    const newGuest = await createGuestUsers({ email });
+    const token = jwt.sign(
+      {
+        email,
+      },
+      JWT_SECRET,
+      { expiresIn: "1w" }
+    );
+    res.send({
+      message: "New user created successfully.",
+      token: token,
+      user: newGuest,
+    });
   } catch (error) {
     next(error);
   }
