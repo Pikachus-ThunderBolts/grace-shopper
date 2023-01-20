e2ba8b55a781d9de6fdcca7b16d139821429857d
Mitchel Alejo <michalejo94@gmail.com>
2023-01-20 11:04:56 -0600

    feat: front-end: added missi ng files and routes.

---
+++ front-end/src/components/Cart.js
@@ -0,0 +1,12 @@
+const Cart = () => {
+  return (
+    <>
+      <section class="section">
+        <h1 class="title">Cart</h1>
+        <h2 class="subtitle">This is where the Cart will live.</h2>
+      </section>
+    </>
+  );
+};
+
+export default Cart;
