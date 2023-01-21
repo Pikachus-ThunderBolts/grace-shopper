32bc83aa6ade0df21b9a20b20792001f06080bb8
Cindy Carrillo <carrillo201994@gmail.com>
2023-01-21 10:52:30 -0600

    feat(api): planning out what api calls we need

---
+++ src/api/api.js
@@ -0,0 +1,54 @@
+const URL = `https://tech-buy-one.onrender.com/api`
+
+/* PRODUCTS */
+//fetchAllProducts
+//fetchProduct
+//createProduct
+//updateProduct
+//deleteProduct
+
+
+/* ADMIN USERS */
+//createAdminUsers
+//fetchAdminUsers
+//registerAdminUsers
+//loginAdminUsers
+
+
+/* GUEST USERS */
+//createGuestUsers
+//fetchGuestUsers
+//registerGuestUsers
+//loginGuestUsers
+
+
+/* CUSTOMER USERS */
+//createCustomerUsers
+//fetchCustomerUsers
+//registerCustomerUsers
+//loginCustomerUsers
+
+
+/* REVIEWS */
+//fetchReviews
+//createReviews
+//patchReviews
+//deleteReviews
+
+
+/* ORDERS */
+//fetchAllOrders
+//fetchOrder
+//fetchCustomerOrder
+//fetchGuestOrder
+//patchOrders
+//deleteOrder
+
+/* CART */
+//fetchCarts
+//fetchGuestCart
+//fetchCustomerCart
+//patchGuestCart
+//patchCustomerCart
+//deleteGuestCart
+//deleteCustomerCart
