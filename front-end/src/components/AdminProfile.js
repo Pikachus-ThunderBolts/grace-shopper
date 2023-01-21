6c214c9462d1b42cd37176b35bea008b400e73dc
Martin Hampton <martinh0205@gmail.com>
2023-01-21 12:37:56 -0600

    components created

--- front-end/src/components/AdminProfile.js
+++ front-end/src/components/AdminProfile.js
@@ -2,10 +2,15 @@ const AdminProfile = () => {
   return (
     <>
       <section class="section">
-        <h1 class="title">Admin Profile</h1>
-        <h2 class="subtitle">
-          This is where admin users will see their profile.
-        </h2>
+        <h1 class="title">Profile</h1>
+        <h2 class="subtitle">Username:</h2>
+        <h2 class="subtitle">Email:</h2>
+      </section>
+      <section class="section has-background-info">
+        <h1 class="title has-text-white">Admin Tools</h1>
+        <h3 className="content has-text-white">Create Product</h3>
+        <h3 className="content has-text-white">Edit Product</h3>
+        <h3 className="content has-text-white">Create Admin User</h3>
       </section>
     </>
   );
