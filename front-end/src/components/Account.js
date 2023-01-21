07d5ed13318f93df2f2b591bf14113c9ec2caa8c
Martin Hampton <martinh0205@gmail.com>
2023-01-21 09:16:48 -0600

    cart ui

--- front-end/src/components/Account.js
+++ front-end/src/components/Account.js
@@ -2,10 +2,31 @@ const Account = () => {
   return (
     <>
       <section class="section">
-        <h1 class="title">Account</h1>
-        <h2 class="subtitle">
-          This is our regular users will sign in/register.
-        </h2>
+        <h1 class="title">Register / Login</h1>
+        <div class="field">
+          <p class="control has-icons-left has-icons-right">
+            <input class="input" type="email" placeholder="Email"></input>
+            <span class="icon is-small is-left">
+              <i class="fas fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fas fa-check"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <p class="control has-icons-left">
+            <input class="input" type="password" placeholder="Password"></input>
+            <span class="icon is-small is-left">
+              <i class="fas fa-lock"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <p class="control">
+            <button class="button is-success">Login</button>
+          </p>
+        </div>
       </section>
     </>
   );
--- front-end/src/components/Admin.js
+++ front-end/src/components/Admin.js
@@ -2,8 +2,31 @@ const Admin = () => {
   return (
     <>
       <section class="section">
-        <h1 class="title">Admin</h1>
-        <h2 class="subtitle">this is where our admin users will login</h2>
+        <h1 class="title">Admin Login</h1>
+        <div class="field">
+          <p class="control has-icons-left has-icons-right">
+            <input class="input" type="email" placeholder="Email"></input>
+            <span class="icon is-small is-left">
+              <i class="fas fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fas fa-check"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <p class="control has-icons-left">
+            <input class="input" type="password" placeholder="Password"></input>
+            <span class="icon is-small is-left">
+              <i class="fas fa-lock"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <p class="control">
+            <button class="button is-success">Login</button>
+          </p>
+        </div>
       </section>
     </>
   );
