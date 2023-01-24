2972603056f4feaa02c26662a06316d2da978d95
Martin Hampton <martinh0205@gmail.com>
2023-01-24 13:41:13 -0600

    reviews working

--- front-end/src/components/ProductDetail.js
+++ front-end/src/components/ProductDetail.js
@@ -1,7 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 const ProductDetail = ({ products, reviews }) => {
+  const [reviewsPage, setReviewsPage] = useState([]);
   const { productId } = useParams();
+
+  function fetchReviewsPage(reviews) {
+    const newReviews = [];
+    for (let i in reviews) {
+      if (productId == reviews[i].productId) {
+        newReviews.push(reviews[i]);
+      }
+    }
+    return newReviews;
+  }
+
+  useEffect(() => {
+    const getReviewsPage = async () => {
+      const reviewsPage = await fetchReviewsPage(reviews);
+
+      setReviewsPage(reviewsPage);
+    };
+    getReviewsPage();
+  }, []);
+
+  console.log("reviewsPage", reviewsPage);
+
   const singleProduct = products.find((oneProduct) => {
     const foundProduct = oneProduct.id == productId;
 
@@ -9,10 +32,12 @@ const ProductDetail = ({ products, reviews }) => {
   });
 
   const singleReview = reviews.find((oneReview) => {
-    const foundReview = oneReview.id == productId;
+    const foundReview = oneReview.productId == productId;
 
     return foundReview;
   });
+
+  singleReview;
 
   if (singleProduct) {
     return (
@@ -21,11 +46,12 @@ const ProductDetail = ({ products, reviews }) => {
           <div className="tile is-ancestor">
             <div class="tile is-parent">
               <article class="tile is-child notification is-white">
-                <p class="title">{singleProduct.title}</p>
-                <p class="subtitle">{singleProduct.price}</p>
+                <p class="title has-text-centered">{singleProduct.title}</p>
+
                 <figure class="image is-4by3">
                   <img src={singleProduct.img}></img>
                 </figure>
+                <p class="subtitle">{singleProduct.price}</p>
                 <p class="content">{singleProduct.brand}</p>
                 <p class="content">{singleProduct.description}</p>
                 <button class="button is-focused">Add to Cart</button>
@@ -34,7 +60,8 @@ const ProductDetail = ({ products, reviews }) => {
           </div>
         </section>
         <section class="section has-background-info">
-          <h1 class="title has-text-white">Leave Reviews</h1>
+          <h1 class="title has-text-white has-text-centered">Leave Reviews</h1>
+
           <br></br>
           <div class="field">
             <label class="label">Review Title</label>
@@ -59,24 +86,22 @@ const ProductDetail = ({ products, reviews }) => {
             </div>
           </div>
           <hr></hr>
-          {singleReview ? (
-            <article class="media box">
-              <div class="media-content">
-                <h1 class="title">Reviews</h1>
-                <hr></hr>
-                <div className="content">
-                  <h1>{singleReview.title}</h1>
-                  <p>{singleReview.review}</p>
-                </div>
+        </section>
+        <section className="section has-background-light">
+          <h1 className="title has-text-centered ">Reviews</h1>
+
+          {reviewsPage.map((individualReview) => {
+            return (
+              <div className="title is-ancestor box">
+                <h1 className="title">{individualReview.title}</h1>
+
+                <p className="subtitle">{individualReview.review}</p>
               </div>
-            </article>
-          ) : null}
+            );
+          })}
         </section>
       </>
     );
-  }
-  if (singleReview) {
-    return <h1>working</h1>;
   }
 };
 
